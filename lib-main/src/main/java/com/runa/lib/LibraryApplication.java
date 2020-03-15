39f07232fec5ce3749cf79ad58dfecfc94e06212
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2020-03-15 22:29:31 +0300

    +

--- lib-main/src/main/java/com/runa/lib/LibraryApplication.java
+++ lib-main/src/main/java/com/runa/lib/LibraryApplication.java
@@ -3,6 +3,7 @@ package com.runa.lib;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LibraryApplication {
 
