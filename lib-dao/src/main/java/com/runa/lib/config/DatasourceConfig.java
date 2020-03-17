798481f413df435563fab6cc14fde10620407169
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2020-03-17 16:12:27 +0300

    +

---
+++ lib-dao/src/main/java/com/runa/lib/config/DatasourceConfig.java
@@ -0,0 +1,47 @@
+package com.runa.lib.config;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.omg.IOP.TransactionService;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class DatasourceConfig {
+    @Bean
+    public DataSource datasource() {
+        return DataSourceBuilder.create()
+          .driverClassName("com.mysql.cj.jdbc.Driver")
+          .url("jdbc:mysql://localhost:3306/library?createDatabaseIfNotExist=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Asia/Jakarta&useSSL=false")
+          .username("root")
+          .password("root")
+          .build(); 
+    }
+    
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+      HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+      vendorAdapter.setGenerateDdl(true);
+
+      LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+      factory.setJpaVendorAdapter(vendorAdapter);
+      factory.setPackagesToScan("com.runa.lib.entities");
+      factory.setDataSource(datasource());
+      return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+      JpaTransactionManager txManager = new JpaTransactionManager();
+      txManager.setEntityManagerFactory((EntityManagerFactory) entityManagerFactory());
+      return txManager;
+    }
+
+    }
+  
